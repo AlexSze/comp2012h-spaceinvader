701762b279793269527920eb8da17c4f6a933703
owen-lee-cogbm <hwolee@connect.ust.hk>
2019-12-10 19:53:13 +0800

    modified values and re-upload

--- src/enemy.h
+++ src/enemy.h
@@ -6,7 +6,7 @@
 class Enemy : public abstractEnemy
 {
 public:
-    explicit Enemy(unsigned int health=1, int speed=10, bool dir=true, QGraphicsItem* parent=nullptr) : abstractEnemy(health, speed, dir) { }
+    explicit Enemy(unsigned int health=1, int speed=5, bool dir=true, QGraphicsItem* parent=nullptr) : abstractEnemy(health, speed, dir) { }
 };
 
 #endif // ENEMY_H
