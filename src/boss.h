114ef8e9a8dce7f998da476549b488a108f5850d
owen-lee-cogbm <hwolee@connect.ust.hk>
2019-12-11 15:37:58 +0800

    added TODO to boss.h

--- src/boss.h
+++ src/boss.h
@@ -7,5 +7,10 @@
 
 class Boss : public abstractEnemy {
 public:
-    explicit Boss(unsigned int health=5, int speed=10, bool dir=true, QGraphicsItem* parent=nullptr) : abstractEnemy(health, speed, dir, parent) { }
+    explicit Boss(unsigned int health=5, int speed=10, bool dir=true, QGraphicsItem* parent=nullptr) : abstractEnemy(health, speed, dir, parent) {
+        // draw the character
+        // TODO: switch the image to boss
+        setPixmap(QPixmap(":/src/images/base_enemy.png"));
+        setTransformOriginPoint(50,50);
+    }
 };
